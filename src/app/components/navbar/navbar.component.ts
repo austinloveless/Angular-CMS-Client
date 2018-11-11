d9d0de8e6c7744d6ffca37d6ae9b4774166ec244
austinloveless <austinloveless5171@gmail.com>
2018-11-11 12:40:06 -0700

    added conditional rendering in navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { PageService } from "../../services/page.service";
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent implements OnInit {
+  pages: any;
 
-  constructor() { }
+  constructor(public pageService: PageService) {}
 
   ngOnInit() {
+    this.pageService.getPages().subscribe(pages => {
+      this.pageService.pagesBS.next(pages);
+      this.pages = this.pageService.pagesBS;
+    });
   }
-
 }
--- src/app/services/page.service.ts
+++ src/app/services/page.service.ts
@@ -1,6 +1,7 @@
 import { Http } from "@angular/http";
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
 import { Injectable } from "@angular/core";
+import "rxjs/add/operator/map";
 
 @Injectable({
   providedIn: "root"
@@ -9,4 +10,8 @@ export class PageService {
   constructor(private http: Http) {}
 
   public pagesBS = new BehaviorSubject<string>(null);
+
+  getPages() {
+    return this.http.get("http://localhost:8000/pages").map(res => res.json());
+  }
 }
